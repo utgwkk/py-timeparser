c7b5e178ef26fc825df1fc603e0ffec99f909247
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-08-19 12:11:11 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='py-timeparser',
+      version='0.1',
+      description='Parse time string (e.g. 1h5min)',
+      author='utgwkk',
+      author_email='utagawa.kiki@gmail.com',
+      url='https://github.com/utgwkk/py-timeparser',
+      py_modules=['timeparser'],
+      install_requires=['six']
+      license='MIT'
+      )

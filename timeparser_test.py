80b6ed9efee1deab00f012241f2f5bcb4b0d106f
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-08-19 11:56:22 +0900

    Translate docstring

--- timeparser_test.py
+++ timeparser_test.py
@@ -5,54 +5,55 @@ from timeparser import parse, ParseError
 
 class TimeParserTest(unittest.TestCase):
     def test_parse_none(self):
-        '''引数に None が渡された場合の parse のテスト。
-        None を返す。
+        '''Tests timeparser.parse() when None is passed.
+        Returns None.
         '''
         self.assertIsNone(parse(None))
 
     def test_parse_parse_second(self):
-        '''秒のみ指定された場合の parse のテスト。
+        '''Tests timeparser.parse() when second is passed.
         '''
         self.assertEqual(parse('15second'), 15)
 
     def test_parse_parse_second_abbrev(self):
-        '''秒のみ指定された場合の parse のテスト。
-        単位が省略形の場合。
+        '''Tests timeparser.parse() when second is passed.
+        When abbreviation.
         '''
         self.assertEqual(parse('15sec'), 15)
         self.assertEqual(parse('15s'), 15)
 
     def test_parse_parse_second_without_unit(self):
-        '''秒のみ指定された場合の parse のテスト。
-        単位がない場合。
+        '''Tests timeparser.parse() when second is passed.
+        When unit is not specified.
+        Treats as second.
         '''
         self.assertEqual(parse('43'), 43)
 
     def test_parse_parse_minute(self):
-        '''分のみ指定された場合の parse のテスト。
+        '''Tests timeparser.parse() when minute is passed.
         '''
         self.assertEqual(parse('4minute'), 60 * 4)
 
     def test_parse_parse_minute_abbrev(self):
-        '''分のみ指定された場合の parse のテスト。
-        単位が省略形の場合。
+        '''Tests timeparser.parse() when minute is passed.
+        When abbreviation.
         '''
         self.assertEqual(parse('3min'), 60 * 3)
         self.assertEqual(parse('1m'), 60 * 1)
 
     def test_parse_parse_hour(self):
-        '''時のみ指定された場合の parse のテスト。
+        '''Tests timeparser.parse() when hour is passed.
         '''
         self.assertEqual(parse('1hour'), 3600 * 1)
 
     def test_parse_parse_hour_abbrev(self):
-        '''時のみ指定された場合の parse のテスト。
-        単位が省略形の場合。
+        '''Tests timeparser.parse() when hour is passed.
+        When abbreviation.
         '''
         self.assertEqual(parse('4h'), 3600 * 4)
 
     def test_parse_parse_time(self):
-        '''秒・分・時が指定された場合の parse のテスト。
+        '''Tests timeparser.parse() when complex time string is passed.
         '''
         self.assertEqual(parse('1h50min'), 3600 * 1 + 60 * 50)
         self.assertEqual(parse('17m31s'), 60 * 17 + 31)
@@ -61,8 +62,8 @@ class TimeParserTest(unittest.TestCase):
         self.assertEqual(parse('2h5sec'), 3600 * 2 + 5)
 
     def test_parse_parse_time_error(self):
-        '''不正な文字列が指定された場合の parse のテスト。
-        例外を送出する。
+        '''Tests timeparser.parse() when invalid string is passed.
+        Throws timeparser.ParseError.
         '''
         self.assertRaises(ParseError, parse, 'hoge')
         self.assertRaises(ParseError, parse, '1year')
